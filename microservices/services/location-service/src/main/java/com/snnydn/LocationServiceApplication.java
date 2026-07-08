a5d2d362875079d28b22e718c2f84941471a16b6
sinan <en.snnaydin@gmail.com>
2026-07-09 00:43:07 +0300

    city management API added.

--- microservices/services/location-service/src/main/java/com/snnydn/LocationServiceApplication.java
+++ microservices/services/location-service/src/main/java/com/snnydn/LocationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.snnydn.location_service;
+package com.snnydn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ microservices/services/location-service/src/main/java/com/snnydn/service/CityService.java
@@ -0,0 +1,24 @@
+package com.snnydn.service;
+
+import com.snnydn.payload.request.CityRequest;
+import com.snnydn.payload.response.CityResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface CityService {
+
+    CityResponse createCity(CityRequest request) throws Exception;
+    CityResponse getCityById(Long id) throws Exception;
+
+    CityResponse updateCity(Long id, CityRequest request) throws Exception;
+    void deleteCity(Long id) throws Exception;
+
+    Page<CityResponse> getAllCities(Pageable pageable);
+    Page<CityResponse> searchCities(String keyword, Pageable pageable);
+    Page<CityResponse> getCitiesByCountyCode(String countryCode, Pageable pageable);
+
+    boolean cityExist(String cityCode);
+
+
+}
